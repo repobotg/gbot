c02e73cf1c82f51cedd3c7c45b9e48e27c51b568
repobotg <repobotg@gmail.com>
2024-11-15 15:56:55 -0300

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,119 @@
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'))
+import { smsg } from './lib/simple.js'
+import fs from 'fs'
+export async function handler(chatUpdate) {
+if (!chatUpdate) {
+return
+}
+if (!chatUpdate || !chatUpdate.messages) {
+return
+} else {
+this.pushMessage(chatUpdate.messages).catch(console.error)
+}
+let m = chatUpdate.messages[chatUpdate.messages.length - 1]
+if (!m) {
+return
+}
+try {
+m = smsg(this, m) || m
+if (!m)
+return
+function saveMessage(msg) {
+if (!msg.message) return
+const messageData = {
+id: msg.key.id,
+messageInfo: {
+key: msg.key,
+message: msg.message,
+messageTimestamp: msg.messageTimestamp,
+pushName: msg.pushName
+}}
+fs.appendFile('messages.jsonl', JSON.stringify(messageData) + '\n', (err) => {
+if (err) {} }) }
+saveMessage(m)
+if (typeof m.text !== 'string') m.text = ''
+let usedPrefix = '.'
+for (let name in global.plugins) {
+let plugin = global.plugins[name]
+if (!plugin) continue
+if (typeof plugin.before === 'function' && await plugin.before.call(this, m, { conn: this })) continue
+let match = m.text.startsWith(usedPrefix) ? m.text.slice(1).trim() : null
+if (!match) continue
+let [command, ...args] = match.split(' ').filter(v => v)
+command = command.toLowerCase()
+let isAccept = plugin.command instanceof RegExp ? plugin.command.test(command) : Array.isArray(plugin.command) ? plugin.command.some(cmd => cmd instanceof RegExp ? cmd.test(command) : cmd === command) : plugin.command === command
+if (!isAccept) continue
+await plugin.call(this, m, { conn: this, text: args.join(' '), args })
+}} catch (e) {
+console.error(e)
+}}
+export async function deleteUpdate(message) {
+const { m, fromMe, id, participant } = message
+if (fromMe) return
+let msg = this.serializeM(this.loadMessage(id))
+if (!msg) return
+let isOnce = msg.mtype == 'viewOnceMessageV2' || msg.mtype == 'viewOnceMessageV2Extension'
+if (isOnce) {
+let media
+let msgg = msg.mtype == 'viewOnceMessageV2' ? msg.message.viewOnceMessageV2.message : msg.message.viewOnceMessageV2Extension.message
+const type = Object.keys(msgg)[0]
+if (msg.mtype == 'viewOnceMessageV2') {
+media = await downloadContentFromMessage(msgg[type], type == 'imageMessage' ? 'image' : 'videoMessage' ? 'video' : 'audio')
+} else {
+media = await downloadContentFromMessage(msgg[type], 'audio')
+}
+let buffer = Buffer.from([])
+for await (const chunk of media) {
+buffer = Buffer.concat([buffer, chunk])
+}
+if (/image|video/.test(type)) {
+const caption = `*━━━ \`𝘼𝙉𝙏𝙄 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍\` ━━━*
+*┃ ViewOnce (eliminado)*
+- *Nombre:* @${participant.split`@`[0]}
+${msg.chat.endsWith('@g.us') ? `- *Grupo:* ${await conn.getName(msg.chat)}` : '- *Chat privado*'}
+${msgg[type].caption ? `- *Texto:* ${msgg[type].caption}` : '- *Texto:* _sin_texto_'}
+ID: ${id}`
+return await conn.sendFile(conn.user.jid, buffer, null, caption, null, null, { mentions: conn.parseMention(caption), quoted: msg })
+} else if (/audio/.test(type)) {
+await conn.reply(conn.user.jid, `*━━━ \`𝘼𝙉𝙏𝙄 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍\` ━━━*
+*┃ ViewOnce (eliminado)*
+- *Nombre:* @${participant.split`@`[0]}
+${msg.chat.endsWith('@g.us') ? `- *Grupo:* ${await conn.getName(msg.chat)}` : '- *Chat privado*'}
+- *Tipo:* Nota de voz🔊
+ID: ${id}`, m, { mentions: [participant], quoted: msg })
+await conn.sendMessage(conn.user.jid, { audio: buffer, ptt: true }, { quoted: msg })
+}}
+let isImageOrVideo = msg.mtype === 'imageMessage' || msg.mtype === 'videoMessage'
+if (msg.text && !isImageOrVideo) {
+const deleteMsg = `*━━━ \`𝘼𝙉𝙏𝙄 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍\` ━━━*
+*┃ Nombre:* @${participant.split`@`[0]}
+${msg.chat.endsWith('@g.us') ? `*┃ Grupo:* ${await conn.getName(msg.chat)}` : '*┃ Chat privado*'}
+- *📝Mensaje:* ${msg.text}
+*━━━ \`𝘼𝙉𝙏𝙄 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍\` ━━━*
+ID: ${id}`
+await this.sendMessage(conn.user.jid, { text: deleteMsg, mentions: conn.parseMention(deleteMsg) }, { quoted: msg })
+} else if (isImageOrVideo) {
+const iOV = `*━━━ \`𝘼𝙉𝙏𝙄 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍\` ━━━*
+*┃ Nombre:* @${participant.split`@`[0]}
+${msg.chat.endsWith('@g.us') ? `*┃ Grupo:* ${await conn.getName(msg.chat)}` : '*┃ Chat privado*'}
+${msg.text ? `- *Texto:* ${msg.text}` : '- *Texto:* _sin_texto_'}
+ID: ${id}`
+let img = await msg.download?.()
+await conn.sendFile(conn.user.jid, img, null, iOV, null, null, { mentions: conn.parseMention(iOV), quoted: msg })
+} else if (msg.mtype === 'stickerMessage') {
+await conn.sendMessage(conn.user.jid, { text: `*━━━ \`𝘼𝙉𝙏𝙄 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍\` ━━━*
+*┃ Nombre:* @${participant.split`@`[0]}
+${msg.chat.endsWith('@g.us') ? `*┃ Grupo:* ${await conn.getName(msg.chat)}` : '*┃ Chat privado*'}
+*┃ Reenviando sticker..*
+*━━━ 👇🏻👇🏻👇🏻👇🏻👇🏻 ━━━*
+ID: ${id}`, mentions: [participant] }, { quoted: msg })
+this.copyNForward(conn.user.jid, msg)
+} else if (!isOnce) {
+await conn.sendMessage(conn.user.jid, { text: `*━━━ \`𝘼𝙉𝙏𝙄 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍\` ━━━*
+*┃ Nombre:* @${participant.split`@`[0]}
+${msg.chat.endsWith('@g.us') ? `*┃ Grupo:* ${await conn.getName(msg.chat)}` : '*┃ Chat privado*'}
+*┃ Reenviando contenido borrado..*
+*━━━ 👇🏻👇🏻👇🏻👇🏻👇🏻 ━━━*
+ID: ${id}`, mentions: [participant] }, { quoted: msg })
+this.copyNForward(conn.user.jid, msg)
+}}
---
+++ main.js
@@ -0,0 +1,146 @@
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
+import { createRequire } from 'module'
+import path, { join } from 'path'
+import {fileURLToPath, pathToFileURL} from 'url'
+import { platform } from 'process'
+import yargs from 'yargs'
+import fs from 'fs'
+import pino from 'pino'
+import { Boom } from '@hapi/boom'
+import { makeWASocket, protoType, serialize } from './lib/simple.js'
+const { DisconnectReason, useMultiFileAuthState, fetchLatestBaileysVersion, makeCacheableSignalKeyStore } = await import('@whiskeysockets/baileys')
+protoType()
+serialize()
+global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') {
+return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString()
+}; global.__dirname = function dirname(pathURL) {
+return path.dirname(global.__filename(pathURL, true))
+}; global.__require = function require(dir = import.meta.url) {
+return createRequire(dir)
+}
+global.timestamp = { start: new Date }
+const __dirname = global.__dirname(import.meta.url)
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+global.prefix = new RegExp('^[' + (opts['prefix'] || '.').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+global.authFile = `session`
+const {state, saveCreds} = await useMultiFileAuthState(global.authFile)
+const {version} = await fetchLatestBaileysVersion()
+let phoneNumber = "59899022028"
+console.info = () => {}
+console.debug = () => {}
+console.warn = () => {}
+console.error = () => {}
+const connectionOptions = {
+logger: pino({ level: 'silent' }),
+browser: ['Ubuntu', 'Chrome', '20.0.04'],
+auth: {
+creds: state.creds,
+keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+},
+markOnlineOnConnect: false,
+syncFullHistory: false,
+version
+}
+global.conn = makeWASocket(connectionOptions)
+if (!fs.existsSync(`./${authFile}/creds.json`)) {
+if (!conn.authState.creds.registered) {
+setTimeout(async () => {
+let codeBot = await conn.requestPairingCode(phoneNumber);
+codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot;
+console.log(`CÓDIGO DE VINCULACIÓN:`, (codeBot));
+}, 2000)
+}}
+conn.isInit = false
+conn.well = false
+async function connectionUpdate(update) {
+const {connection, lastDisconnect, isNewLogin} = update
+global.stopped = connection
+if (isNewLogin) conn.isInit = true
+const code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
+if (code && code !== DisconnectReason.loggedOut && conn?.ws.socket == null) {
+await global.reloadHandler(true).catch(console.error)
+global.timestamp.connect = new Date
+}
+if (connection == 'open') {
+console.log(`\n🟢 Se ha conectado con WhatsApp.\n`)}
+let reason = new Boom(lastDisconnect?.error)?.output?.statusCode
+if (connection === 'close') {
+if (reason === DisconnectReason.badSession) {
+console.log(`\n⚠️ SIN CONEXIÓN, BORRE LA CARPETA ${global.authFile} ⚠️\n`)
+} else if (reason === DisconnectReason.connectionClosed) {
+console.log(`\n⚠️ CONEXION CERRADA, RECONECTANDO...\n`)
+await global.reloadHandler(true).catch(console.error)
+} else if (reason === DisconnectReason.connectionLost) {
+console.log(`\n⚠️ CONEXIÓN PERDIDA CON EL SERVIDOR, RECONECTANDO...\n`)
+await global.reloadHandler(true).catch(console.error)
+} else if (reason === DisconnectReason.connectionReplaced) {
+console.log(`\n⚠️ CONEXIÓN REEMPLAZADA, SE HA ABIERTO OTRA NUEVA SESION, POR FAVOR, CIERRA LA SESIÓN ACTUAL PRIMERO.\n`)
+} else if (reason === DisconnectReason.loggedOut) {
+console.log(`\n⚠️ SIN CONEXIÓN, BORRE LA CARPETA ${global.authFile} ⚠️\n`)
+await global.reloadHandler(true).catch(console.error)
+} else if (reason === DisconnectReason.restartRequired) {
+console.log(`\nCONECTANDO AL SERVIDOR...\n`)
+await global.reloadHandler(true).catch(console.error)
+} else if (reason === DisconnectReason.timedOut) {
+console.log(`\n⌛ TIEMPO DE CONEXIÓN AGOTADO, RECONECTANDO...\n`)
+await global.reloadHandler(true).catch(console.error)
+} else {
+console.log(`\n⚠️❗ RAZON DE DESCONEXIÓN DESCONOCIDA: ${reason || ''} >> ${connection || ''}\n`)
+}}}
+process.on('uncaughtException', console.error)
+let isInit = true
+let handler = await import('./handler.js')
+global.reloadHandler = async function(restatConn) {
+if (restatConn) {
+const oldChats = global.conn.chats
+try {
+global.conn.ws.close()
+} catch {}
+conn.ev.removeAllListeners()
+global.conn = makeWASocket(connectionOptions, {chats: oldChats})
+isInit = true
+}
+if (!isInit) {
+conn.ev.off('messages.upsert', conn.handler)
+conn.ev.off('message.delete', conn.onDelete)
+conn.ev.off('connection.update', conn.connectionUpdate)
+conn.ev.off('creds.update', conn.credsUpdate)
+}
+conn.handler = handler.handler.bind(global.conn)
+conn.onDelete = handler.deleteUpdate.bind(global.conn)
+conn.connectionUpdate = connectionUpdate.bind(global.conn)
+conn.credsUpdate = saveCreds.bind(global.conn, true)
+conn.ev.on('messages.upsert', conn.handler)
+conn.ev.on('message.delete', conn.onDelete)
+conn.ev.on('connection.update', conn.connectionUpdate)
+conn.ev.on('creds.update', conn.credsUpdate)
+isInit = false
+return true
+}
+const pluginFolder = global.__dirname(join(__dirname, './plugins/index'))
+const pluginFilter = (filename) => /\.js$/.test(filename)
+global.plugins = {}
+async function filesInit() {
+for (const filename of fs.readdirSync(pluginFolder).filter(pluginFilter)) {
+try {
+const file = global.__filename(join(pluginFolder, filename))
+const module = await import(file)
+global.plugins[filename] = module.default || module
+} catch (e) {
+conn.logger.error(e)
+delete global.plugins[filename]
+}}}
+filesInit().then((_) => Object.keys(global.plugins)).catch(console.error)
+Object.freeze(global.reload)
+await global.reloadHandler()
+function clearTmp() {
+const tmpDir = join(__dirname, 'tmp')
+const filenames = fs.readdirSync(tmpDir)
+filenames.forEach(file => {
+const filePath = join(tmpDir, file)
+fs.unlinkSync(filePath)})
+}
+setInterval(async () => {
+if (stopped === 'close' || !conn || !conn.user) return
+await clearTmp()
+}, 1000 * 60 * 60)
