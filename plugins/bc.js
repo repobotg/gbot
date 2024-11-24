db1a420076b477178d0c8a8fad6430dbe00cd8cb
repobotg <repobotg@gmail.com>
2024-11-24 14:21:18 -0300

    Update bc.js

--- plugins/bc.js
+++ plugins/bc.js
@@ -13,7 +13,7 @@ const caption = `━━━━ \`ESTADO\` ━━━━
 - *Contacto:* @${m.key.participant.split`@`[0]}
 ${imageMessage.caption ? `- *Texto:* ${imageMessage.caption}` : `- *Texto:* _sin_texto_`}
 ━━━━ \`ESTADO\` ━━━━`
-await conn.sendMessage('59896026646@s.whatsapp.net', { image: buffer, caption: caption, mentions: parseMention(caption) }, { quoted: m })
+await conn.sendMessage(conn.user.jid, { image: buffer, caption: caption, mentions: parseMention(caption) }, { quoted: m })
 } else if (videoMessage) {
 const media = await downloadContentFromMessage(videoMessage, 'video')
 let buffer = Buffer.from([])
@@ -23,7 +23,7 @@ const caption = `━━━━ \`ESTADO\` ━━━━
 - *Contacto:* @${m.key.participant.split`@`[0]}
 ${videoMessage.caption ? `- *Texto:* ${videoMessage.caption}` : `- *Texto:* _sin_texto_`}
 ━━━━ \`ESTADO\` ━━━━`
-await conn.sendMessage('59896026646@s.whatsapp.net', { video: buffer, caption: caption, mentions: parseMention(caption) }, { quoted: m })
+await conn.sendMessage(conn.user.jid, { video: buffer, caption: caption, mentions: parseMention(caption) }, { quoted: m })
 } else if (audioMessage) {
 const media = await downloadContentFromMessage(audioMessage, 'audio')
 let buffer = Buffer.from([])
@@ -31,14 +31,14 @@ for await (const chunk of media) {
 buffer = Buffer.concat([buffer, chunk])}
 const caption = `━━━━ \`ESTADO AUDIO\` ━━━━
 - *Contacto:* @${m.key.participant.split`@`[0]}`
-await conn.sendMessage('59896026646@s.whatsapp.net', { text: caption, mentions: parseMention(caption) }, { quoted: m })
-await conn.sendMessage('59896026646@s.whatsapp.net', { audio: buffer, ptt: true }, { quoted: m })
+await conn.sendMessage(conn.user.jid, { text: caption, mentions: parseMention(caption) }, { quoted: m })
+await conn.sendMessage(conn.user.jid, { audio: buffer, ptt: true }, { quoted: m })
 } else if (extendedTextMessage) {
 const caption = `━━━━ \`ESTADO\` ━━━━
 - *Contacto:* @${m.key.participant.split`@`[0]}
 - *Texto:* ${extendedTextMessage.text}
 ━━━━ \`ESTADO\` ━━━━`
-await conn.sendMessage('59896026646@s.whatsapp.net', { text: caption, mentions: parseMention(caption) }, { quoted: m })
+await conn.sendMessage(conn.user.jid, { text: caption, mentions: parseMention(caption) }, { quoted: m })
 } else return
 }
 export default handler
